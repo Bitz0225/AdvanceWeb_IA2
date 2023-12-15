b4135805fa76abbdd81cd8ab3ff09ef33219dc79
thaipq <pqthai2502@gmail.com>
2023-12-15 19:55:42 +0700

    test demo GA

--- src/App.js
+++ src/App.js
@@ -1,56 +1,65 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/react-in-jsx-scope */
-import { Form } from 'react-bootstrap';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import axios from 'axios';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { Form } from "react-bootstrap";
+import { useCallback, useEffect, useRef, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import useAnalyticsEventTracker from "./index.js";
 
-const API_URL = 'https://api.unsplash.com';
+const API_URL = "https://api.unsplash.com";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
-  const searchInput = useRef(null)
-  const [images, setImages] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [totalPages, setTotalPages] = useState(null)
-  
-  const getImages = useCallback(async () => { 
+  const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
+
+  const getImages = useCallback(async () => {
     try {
       if (searchInput.current.value) {
-          const { data } = await axios.get(`${API_URL}/search/photos/?query=${searchInput.current.value}&page=${currentPage}&per_page=24&client_id=${API_KEY}`)
-          setImages([...images, ...data.results])
-          setTotalPages(data.total_pages)
-        }
-      } catch (error) {
-        
+        const { data } = await axios.get(
+          `${API_URL}/search/photos/?query=${searchInput.current.value}&page=${currentPage}&per_page=24&client_id=${API_KEY}`
+        );
+        setImages([...images, ...data.results]);
+        setTotalPages(data.total_pages);
       }
-  }, [currentPage])
+    } catch (error) {}
+  }, [currentPage]);
 
-  const handleSearch = useCallback((e) => { 
-    e.preventDefault();
-    setImages([]);
-    setCurrentPage(1);
-    getImages();
-  }, [searchInput])
-  
+  const gaEventTracker = useAnalyticsEventTracker("Images Search");
+
+  const handleSearch = useCallback(
+    (e) => {
+      gaEventTracker("Search", searchInput.current.value);
+      e.preventDefault();
+      setImages([]);
+      setCurrentPage(1);
+      getImages();
+    },
+    [searchInput]
+  );
+
   useEffect(() => {
-    getImages()
-  }, [getImages])
+    getImages();
+  }, [getImages]);
 
-  useEffect(() => { setImages([]) }, [handleSearch])
-  
+  useEffect(() => {
+    setImages([]);
+  }, [handleSearch]);
+
   return (
     <div className="container">
-      <h1 className='title'>Images Search</h1>
-      <div className='search-section'>
+      <h1 className="title">Images Search</h1>
+      <div className="search-section">
         <Form onSubmit={handleSearch}>
           <Form.Control
-            type='search'
-            placeholder='Search for images'
-            className='search-input'
+            type="search"
+            placeholder="Search for images"
+            className="search-input"
             ref={searchInput}
           />
         </Form>
@@ -60,15 +69,22 @@ function App() {
         next={() => setTimeout(() => setCurrentPage(currentPage + 1), 500)}
         hasMore={currentPage < totalPages}
         loader={
-          <div className='loading'>
-            <div className="lds-ripple"><div></div><div></div></div>
+          <div className="loading">
+            <div className="lds-ripple">
+              <div></div>
+              <div></div>
+            </div>
           </div>
         }
       >
-        <div className='images-section'>
+        <div className="images-section">
           {images.map((image) => (
-            <div className='image'>
-              <img key={image.id} src={image.urls.regular} alt={image.alt_description} />
+            <div className="image">
+              <img
+                key={image.id}
+                src={image.urls.regular}
+                alt={image.alt_description}
+              />
             </div>
           ))}
         </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ReactGA from "react-ga4";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-DTDNFDRG6Z");
+
+const useAnalyticsEventTracker = (category) => {
+  const eventTracker = (action = "test action", label = "test label") => {
+    ReactGA.event({ category, action, label });
+  };
+  return eventTracker;
+};
+export default useAnalyticsEventTracker;
+
+ReactGA.send("pageview");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
