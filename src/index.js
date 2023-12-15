23fdccce64efefb1da43c2271097d95be5bfd22c
thaipq <pqthai2502@gmail.com>
2023-12-15 21:41:14 +0700

    fix injected file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ReactGA from "react-ga4";
 import { Analytics } from "@vercel/analytics/react";
-
-ReactGA.initialize("G-DTDNFDRG6Z");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
